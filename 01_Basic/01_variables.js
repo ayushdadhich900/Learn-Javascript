7cb8c871b67fd4c5d3b6b2a78505b5d8e4275263
ayushdadhich900 <128716968+ayushdadhich900@users.noreply.github.com>
2024-07-26 06:23:39 +0000

    variable config done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 245589
+let accountEmail = "ayush900@gmail.com"
+var accountpass = "123456"
+accountCity = "JAIPUR"
+let accountState;
+
+// accountId  = 2
+accountpass = "145"
+accountEmail = "gahha@gmail.com"
+accountCity = "pune"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountpass,accountCity,accountState])
+
+//  Prefer not to use var
+// because of issue in block scope and functionl acope
